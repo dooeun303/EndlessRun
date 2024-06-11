c03f056ce5e061bd43b5036f7609533219d123af
dooeun303 <116616865+dooeun303@users.noreply.github.com>
2024-06-11 14:17:25 +0900

    초기설정, 달리기기능

---
+++ Assets/02_Scripts/PlayerMove.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 10f;
+    public float jumpHeight = 3f;
+    public float rotSpeed = 3f;
+
+    private Animator anim;
+
+    private Vector3 dir = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir.x = Input.GetAxis("Horizontal");
+        dir.z = Input.GetAxis("Vertical");
+        dir.Normalize(); // 대각선 으로 갈때도 속도 동일하게 함
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (dir != Vector3.zero) 
+        {
+            anim.SetBool("Run", true);
+            // 지금 바라보는 방향의 부호 != 나아갈 방향 부호 (정반대 방향볼때 버벅이는거 해결)
+            if (Mathf.Sign(transform.forward.x) != Mathf.Sign(dir.x) || Mathf.Sign(transform.forward.z) != Mathf.Sign(dir.z))
+
+                {
+                    transform.Rotate(0, 1, 0);
+            }
+            transform.forward = Vector3.Lerp(transform.forward, dir, rotSpeed * Time.deltaTime);
+        }
+        else
+        {
+            anim.SetBool("Run", false);
+        }
+
+        rb.MovePosition(this.gameObject.transform.position + dir * speed * Time.deltaTime);
+    }
+}
